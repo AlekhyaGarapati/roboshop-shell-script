3023d116e9d2de1083c0645f3e448fa978ee6b68
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-26 18:27:40 +0530

    ec2-instances

--- Automate-ec2-route53-creation.sh
+++ Automate-ec2-route53-creation.sh
@@ -2,6 +2,6 @@
 
 NAME="DevOPS"
 PRIVATE_IPADDRESS=""
-PRIVATE_IPADDRESS=$(aws ec2 run-instances --image-id ami-081609eef2e3cc958 --instance-type t2.micro --security-group-ids sg-00b35098594b5d64c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIPAddress')
+PRIVATE_IPADDRESS=$(aws ec2 run-instances --image-id ami-081609eef2e3cc958 --instance-type t2.micro --security-group-ids sg-00b35098594b5d64c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]" | jq -r '.Instances[0].PrivateIPAddress')
 
 echo "PRIVATE_IPADDRESS:= $PRIVATE_IPADDRESS"

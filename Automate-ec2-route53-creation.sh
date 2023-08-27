528488c6af67f2019303e1c64a880545300415b3
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-27 19:00:17 +0530

    ec2-instances

--- Automate-ec2-route53-creation.sh
+++ Automate-ec2-route53-creation.sh
@@ -1,7 +1,29 @@
 #!/bn/bash
 
-NAME="DevOPS"
-PRIVATE_IPADDRESS=""
-PRIVATE_IPADDRESS=$(aws ec2 run-instances --image-id ami-081609eef2e3cc958 --instance-type t2.micro --security-group-ids sg-00b35098594b5d64c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]" | jq -r '.Instances[0].PrivateIPAddress')
+NAME=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-00b35098594b5d64c
+DOMAIN_NAME=robokart.online
+HOSTED_ZONE=Z07719251CXO88RVYQRQR
+  
+for i in "${NAMES[@]}"
+do  
+    echo "creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo "created $i instance: $IP_ADDRESS"
 
-echo "PRIVATE_IPADDRESS:= $PRIVATE_IPADDRESS"
+    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+done
+

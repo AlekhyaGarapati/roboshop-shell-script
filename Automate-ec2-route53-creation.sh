5664712429ee0c3e8518332283fd8bb3ca4b0616
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-27 19:06:15 +0530

    commit

--- Automate-ec2-route53-creation.sh
+++ Automate-ec2-route53-creation.sh
@@ -1,5 +1,5 @@
 #!/bn/bash
-
+echo "Hi"
 NAME=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb

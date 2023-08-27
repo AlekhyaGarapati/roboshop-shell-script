6b2f8947870691025be144728547f83f1e5b11bc
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-27 19:31:24 +0530

    commit

--- Automate-ec2-route53-creation.sh
+++ Automate-ec2-route53-creation.sh
@@ -10,7 +10,7 @@ HOSTED_ZONE=Z07719251CXO88RVYQRQR
   for i in "${NAME[@]}"
   do 
    echo "Creating $i instance"
-IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $sg-00b35098594b5d64c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
 
    echo "Created $i instance :$IP_ADDRESS"
   done

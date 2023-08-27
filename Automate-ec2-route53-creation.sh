459104b3099f243130aec4ad3920cefd901b5b0f
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-27 19:12:40 +0530

    commit

--- Automate-ec2-route53-creation.sh
+++ Automate-ec2-route53-creation.sh
@@ -1,7 +1,7 @@
 #!/bn/bash
 echo "Hi"
 NAME=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-INSTANCE_TYPE="t2.micro"
+INSTANCE_TYPE=t2.micro
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-00b35098594b5d64c
 DOMAIN_NAME=robokart.online

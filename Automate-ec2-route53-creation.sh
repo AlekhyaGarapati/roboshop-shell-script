bbf76383ab1663b5b785e0df82508d752945495d
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-27 19:21:11 +0530

    commit

--- Automate-ec2-route53-creation.sh
+++ Automate-ec2-route53-creation.sh
@@ -1,5 +1,5 @@
 #!/bn/bash
-echo "Hi"
+
 NAME=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=t2.micro
 IMAGE_ID=ami-03265a0778a880afb
@@ -7,23 +7,8 @@ SECURITY_GROUP_ID=sg-00b35098594b5d64c
 DOMAIN_NAME=robokart.online
 HOSTED_ZONE=Z07719251CXO88RVYQRQR
   
-for i in "${NAMES[@]}"
-do  
-    echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-    echo "created $i instance: $IP_ADDRESS"
-
-    aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch '
-    {
-            "Changes": [{
-            "Action": "CREATE",
-                        "ResourceRecordSet": {
-                            "Name": "'$i.$DOMAIN_NAME'",
-                            "Type": "A",
-                            "TTL": 300,
-                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-                        }}]
-    }
-    '
-done
+  for i in "${NAME[@]}"
+  do 
+   echo "Name:$NAME[i]"
+  done
 

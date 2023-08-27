c532255a0748ba5b9f1ddedf56f66efbcb6bcbc3
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-27 20:00:34 +0530

    commit

--- Automate-ec2-route53-creation.sh
+++ Automate-ec2-route53-creation.sh
@@ -1,7 +1,7 @@
 #!/bn/bash
 
 #NAME=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-NAME=("mongodb" "redis")
+NAME=("cab" "bac")
 INSTANCE_TYPE=t2.micro
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-00b35098594b5d64c
@@ -12,7 +12,7 @@ HOSTED_ZONE=Z07719251CXO88RVYQRQR
   do 
    echo "Creating $i instance"
 IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=$i,Values=running")
+INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=$i")
    echo "Created $i instance :$IP_ADDRESS"
    echo "Created $i instance :$INSTANCE_ID"
   done

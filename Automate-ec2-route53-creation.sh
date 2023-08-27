d979bf2b0d7de6bd4d0645553be55b85aa356a71
AlekhyaGarapati <alekhyaranigarapati@gmail.com>
2023-08-27 21:51:50 +0530

    commit

--- Automate-ec2-route53-creation.sh
+++ Automate-ec2-route53-creation.sh
@@ -1,7 +1,6 @@
 #!/bn/bash
 
-#NAME=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-NAME=("cab" "bac")
+NAME=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=t2.micro
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-00b35098594b5d64c
@@ -12,8 +11,20 @@ HOSTED_ZONE=Z07719251CXO88RVYQRQR
   do 
    echo "Creating $i instance"
 IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
-INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=$i")
    echo "Created $i instance :$IP_ADDRESS"
-   echo "Created $i instance :$INSTANCE_ID"
+
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                            "Name": "'$i.$DOMAIN_NAME'",
+                            "Type": "A",
+                            "TTL": 300,
+                            "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                        }}]
+    }
+    '
+    echo "Domain Name Server created for $i :$i.$SOMAIN_NAME"
   done
 
